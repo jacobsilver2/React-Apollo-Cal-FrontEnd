07390a5909119d1e6831abe944eba04dc4b425af
jacobsilver2 <jacobsilver2@mac.com>
2018-11-20 15:01:35 -0500

    added signup page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6cb0ec686a1f5296eaa0";
+/******/ 	var hotCurrentHash = "c7627c7fc77ff41bb5bc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
